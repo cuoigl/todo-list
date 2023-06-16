2d2ded54f53b10d50eb3d9d50f682640580020bb
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-06-16 14:29:48 +0700

    Style

--- Components/Todo.js
+++ Components/Todo.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
-import { View, Text, Button } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const Todo = (props) => {
-  const [title, setTitle] = useState("TodoList");
   return (
-    <View>
+    <View style={[styles.item, { margin: 8, padding: 8 }]}>
       <Text>{props.name}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColorl: "whitesmoke",
+  },
+});
 
 export default Todo;
--- Components/TodoList.js
+++ Components/TodoList.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import Todo from "./Todo";
 
 const TodoList = () => {
   const [title, setTitle] = useState("TodoList");
   return (
-    <View>
-      <Text>{title}</Text>
-      <Todo name={"Frist Todo"} />
+    <View style={{ width: "80%", marginBottom: 60 }}>
+      <Text style={[styles.align, styles.font]}>{title}</Text>
+      <Todo name={"First Todo"} />
       <Todo name={"Second Todo"} />
       <Button title="Change me" onPress={() => setTitle("My List")} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  align: {
+    alignSelf: "center",
+  },
+  font: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
 
 export default TodoList;
